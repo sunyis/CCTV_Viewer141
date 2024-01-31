8879d28ff904b2c3a9a0914265f9a4589c957298
lizhi <405628079@qq.com>
2024-01-31 20:13:10 +0800

    优化代码，设置安卓打包环境

--- app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
+++ app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.eanyatonic.cctvViewer;
 
 import static com.eanyatonic.cctvViewer.FileUtils.copyAssets;
+import static com.eanyatonic.cctvViewer.TVUrls.channelNames;
+import static com.eanyatonic.cctvViewer.TVUrls.liveUrls;
 
 import static java.lang.Thread.sleep;
 import android.net.http.SslError;
@@ -34,93 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView; // 导入 X5 WebView
 
-    private String[] liveUrls = {
-            "https://tv.cctv.com/live/cctv1/",
-            "https://tv.cctv.com/live/cctv2/",
-            "https://tv.cctv.com/live/cctv3/",
-            "https://tv.cctv.com/live/cctv4/",
-            "https://tv.cctv.com/live/cctv5/",
-            "https://tv.cctv.com/live/cctv6/",
-            "https://tv.cctv.com/live/cctv7/",
-            "https://tv.cctv.com/live/cctv8/",
-            "https://tv.cctv.com/live/cctvjilu",
-            "https://tv.cctv.com/live/cctv10/",
-            "https://tv.cctv.com/live/cctv11/",
-            "https://tv.cctv.com/live/cctv12/",
-            "https://tv.cctv.com/live/cctv13/",
-            "https://tv.cctv.com/live/cctvchild",
-            "https://tv.cctv.com/live/cctv15/",
-            "https://tv.cctv.com/live/cctv16/",
-            "https://tv.cctv.com/live/cctv17/",
-            "https://tv.cctv.com/live/cctv5plus/",
-            "https://tv.cctv.com/live/cctveurope",
-            "https://tv.cctv.com/live/cctvamerica/",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002309",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002521",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002483",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002520",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002475",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002508",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002485",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002509",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002498",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002506",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002531",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002481",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002516",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002525",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002484",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002490",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002503",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002505",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002532",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002493",
-            "https://www.yangshipin.cn/#/tv/home?pid=600002513",
-    };
 
-    private String[] channelNames = {
-            "1 CCTV-1 综合",
-            "2 CCTV-2 财经",
-            "3 CCTV-3 综艺",
-            "4 CCTV-4 中文国际",
-            "5 CCTV-5 体育",
-            "6 CCTV-6 电影",
-            "7 CCTV-7 军事农业",
-            "8 CCTV-8 电视剧",
-            "9 CCTV-9 纪录",
-            "10 CCTV-10 科教",
-            "11 CCTV-11 戏曲",
-            "12 CCTV-12 社会与法",
-            "13 CCTV-13 新闻",
-            "14 CCTV-14 少儿",
-            "15 CCTV-15 音乐",
-            "16 CCTV-16 奥林匹克",
-            "17 CCTV-17 农业农村",
-            "18 CCTV-5+ 体育赛事",
-            "19 CCTV Europe",
-            "20 CCTV America",
-            "21 北京卫视",
-            "22 江苏卫视",
-            "23 东方卫视",
-            "24 浙江卫视",
-            "25 湖南卫视",
-            "26 湖北卫视",
-            "27 广东卫视",
-            "28 广西卫视",
-            "29 黑龙江卫视",
-            "30 海南卫视",
-            "31 重庆卫视",
-            "32 深圳卫视",
-            "33 四川卫视",
-            "34 河南卫视",
-            "35 福建东南卫视",
-            "36 贵州卫视",
-            "37 江西卫视",
-            "38 辽宁卫视",
-            "39 安徽卫视",
-            "40 河北卫视",
-            "41 山东卫视",
-    };
 
     private int currentLiveIndex;
 
@@ -295,7 +211,7 @@ public class MainActivity extends AppCompatActivity {
         overlayTextView = findViewById(R.id.overlayTextView);
 
         // 加载上次保存的位置
-        loadLastLiveIndex();
+        //loadLastLiveIndex();
 
 
 
@@ -321,10 +237,10 @@ public class MainActivity extends AppCompatActivity {
                 handler.proceed(); // 忽略 SSL 错误
             }
 
-            // 设置 WebViewClient，监听页面加载完成事件
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                    doFullScren(view,true);
+                // 设置 WebViewClient，监听页面加载完成事件
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    //doFullScren(view,true);
                 }
             });
 
@@ -345,7 +261,7 @@ public class MainActivity extends AppCompatActivity {
         loadLiveUrl();
 
         // 启动定时任务，每隔一定时间执行一次
-        startPeriodicTask();
+        //startPeriodicTask();
     }
 
     // 频道选择列表
@@ -618,7 +534,7 @@ public class MainActivity extends AppCompatActivity {
     private void loadLiveUrl() {
         if (currentLiveIndex >= 0 && currentLiveIndex < liveUrls.length) {
             // 显示加载的View
-            loadingOverlay.setVisibility(View.VISIBLE);
+            //loadingOverlay.setVisibility(View.VISIBLE);
 
             webView.setInitialScale(getMinimumScale());
             webView.loadUrl(liveUrls[currentLiveIndex]);
